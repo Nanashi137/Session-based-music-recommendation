36215fb9ab3e8c0e602b1b73b6185d0b68de5c6f
Nanashi137 <c0mputereng1neerstudent0@gmail.com>
2025-05-07 08:36:40 +0700

    Added recsys torch dataset

--- torch_dataset/recsys_dataset.py
+++ torch_dataset/recsys_dataset.py
@@ -4,14 +4,20 @@ from torch.utils.data import Dataset
 import polars as pl
 import numpy as np
 
-class Recsys_dataset(Dataset):
+class RecsysDataset(Dataset):
     def __init__(self, data_path:str):
         super().__init__()
 
-        self.df = pl.read_csv(data_path)
+        df = pl.read_csv(data_path).drop("session_id")
+
+        self.sequences = df['sequence'].to_list()
+        self.targets = df['target'].to_list()
 
     def __len__(self):
-        return len(self.df)
+        return len(self.targets)
     
     def __getitem__(self, idx):
-        
+        sequence = (lambda x: [int(i) for i in x])(self.sequences[idx].split("-"))
+        target = int(self.targets[idx])
+
+        return sequence, len(sequence), target
