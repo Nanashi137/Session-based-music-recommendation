25d896a30b908ca17971f99d77d60147ca26a479
Nanashi137 <c0mputereng1neerstudent0@gmail.com>
2025-05-08 16:22:09 +0700

    Minor changes

--- torch_dataset/collator.py
+++ torch_dataset/collator.py
@@ -10,26 +10,13 @@ class RecsysCollateFunction:
         longest_len = max(seq_lens)
         [seq.extend([self.padding_idx]*(longest_len-len(seq))) for seq in seqs] # padding
 
-        seqs = torch.tensor(seqs, dtype=torch.int32)
-        seq_lens = torch.tensor(seq_lens, dtype=torch.int32)
-        targets = torch.tensor(targets, dtype=torch.int32)
+        seqs = torch.tensor(seqs)
+        seq_lens = torch.tensor(seq_lens)
+        targets = torch.tensor(targets)
 
         return seqs, seq_lens, targets
 
 
-if __name__ == "__main__":
-    seqs = [[1,2,3,4],[1,2,3],[1]]
-    seq_lens = [4, 3, 1]
-
-    longest_len = max(seq_lens)
-    [seq.extend([3773]*(longest_len-len(seq))) for seq in seqs] # padding
-
-    seqs = torch.tensor(seqs, dtype=torch.int32)
-    seq_lens = torch.tensor(seq_lens, dtype=torch.int32)
-
-    print(seqs)
-    print(seq_lens)
-    
 
 
 
